5c6718bc8a981b5b782a7f54fff3a8709996e51a
Luca Taschner <luca@taschner.me>
2022-11-18 13:26:05 +0100

    Hooked the lounge system to the sqlite database for better handeling

--- NinjaBot-DC/Extensions/LoungeSystem.cs
+++ NinjaBot-DC/Extensions/LoungeSystem.cs
@@ -1,6 +1,9 @@
-﻿using DSharpPlus;
+﻿using Dapper;
+using Dapper.Contrib.Extensions;
+using DSharpPlus;
 
 using DSharpPlus.EventArgs;
+using NinjaBot_DC.Models;
 
 namespace NinjaBot_DC.Extensions;
 
@@ -8,7 +11,7 @@ public static class LoungeSystem
 {
     public static async Task VoiceStateUpdated_ChanelEnter(DiscordClient client, VoiceStateUpdateEventArgs eventArgs)
     {
-        if ( eventArgs.Channel == null || eventArgs.Channel.Id != 1040993809368109066)
+        if ( eventArgs.Channel == null || eventArgs.Channel.Name != "╠👥»Join 2 Create")
             return;
 
 
@@ -17,11 +20,25 @@ public static class LoungeSystem
             
         if (newChannel == null)
             return;
-            
+        
         eventArgs.Channel.Guild.Members.TryGetValue(eventArgs.User.Id, out var discordMember);
         if (discordMember == null)
             return;
+        
+        var newModel = new Models.LoungeDbModel {ChannelId = newChannel.Id, OwnerId =discordMember.Id, GuildId = eventArgs.Guild.Id};
+        
+        var updated = await Worker.SqLiteConnection.InsertAsync(newModel);
+
+        if (updated == 0)
+        {
+            await Task.Run(async () =>
+            {
+                await newChannel.DeleteAsync();
+            });
             
+            return;
+        }
+        
         await  Task.Run(async () =>
         {
             await discordMember.PlaceInAsync(newChannel);
@@ -49,15 +66,44 @@ public static class LoungeSystem
     
     public static async Task VoiceStateUpdated_ChanelLeave(DiscordClient client, VoiceStateUpdateEventArgs eventArgs)
     {
-        if ( eventArgs.Before == null || !eventArgs.Before.Channel.Name.Contains("🥳"))
+        if ( eventArgs.Before == null)
             return;
-            
-        if (eventArgs.Before.Channel.Users.Count != 0)
-            return;
+        
 
-        await Task.Run(async () =>
+        var loungeList = await Worker.SqLiteConnection.GetAllAsync<LoungeDbModel>();
+
+        foreach (var loungeModel in loungeList)
         {
-            await eventArgs.Before.Channel.DeleteAsync();
-        });
+            if (loungeModel.ChannelId != eventArgs.Before.Channel.Id)
+                continue;
+            
+            if (eventArgs.Before.Channel.Users.Count != 0)
+                return;
+
+            await Task.Run(async () =>
+            {
+                await eventArgs.Before.Channel.DeleteAsync();
+                await Worker.SqLiteConnection.DeleteAsync(loungeModel);
+            });
+        }
+        
+
+    }
+
+    public static async Task StartupCleanup(DiscordClient discordClient)
+    {
+        var loungeDbModels = await Worker.SqLiteConnection.GetAllAsync<LoungeDbModel>();
+
+        foreach (var loungeDbModel in loungeDbModels)
+        {
+            var loungeChannel = await discordClient.GetChannelAsync(loungeDbModel.ChannelId);
+            var guild = await discordClient.GetGuildAsync(loungeDbModel.GuildId);
+            
+            //For some reason in this case the user count has to be queried by the guild not the channel because that would often result in an exception 
+            if (guild.Channels[loungeDbModel.ChannelId].Users.Count == 0)
+                await loungeChannel.DeleteAsync();
+
+            await Worker.SqLiteConnection.DeleteAsync(loungeDbModel);
+        }
     }
 }
---
+++ NinjaBot-DC/Models/LoungeDbModel.cs
@@ -0,0 +1,13 @@
+﻿using Dapper.Contrib.Extensions;
+
+namespace NinjaBot_DC.Models;
+
+[System.ComponentModel.DataAnnotations.Schema.Table(("LoungeIndex"))]
+public class LoungeDbModel
+{
+    [ExplicitKey]
+    public ulong ChannelId { get; set; }
+    public ulong OwnerId { get; set; }
+    
+    public ulong GuildId { get; set; }
+}

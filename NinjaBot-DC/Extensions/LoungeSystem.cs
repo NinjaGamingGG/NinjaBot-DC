fd46267cce9d1b2645ce7234ba21c2158bea7a5b
Luca Taschner <luca@taschner.me>
2022-11-18 11:08:49 +0100

    Changed the VoiceStateUpdated_ChanelEnter event method a bit so hopefully there will be no more "An event handler took too long to execute" warnings

--- NinjaBot-DC/Extensions/LoungeSystem.cs
+++ NinjaBot-DC/Extensions/LoungeSystem.cs
@@ -1,7 +1,5 @@
 ﻿using DSharpPlus;
-using DSharpPlus.CommandsNext;
-using DSharpPlus.CommandsNext.Attributes;
-using DSharpPlus.Entities;
+
 using DSharpPlus.EventArgs;
 
 namespace NinjaBot_DC.Extensions;
@@ -13,24 +11,40 @@ public static class LoungeSystem
         if ( eventArgs.Channel == null || eventArgs.Channel.Id != 1040993809368109066)
             return;
 
+
+        var newChannel = await eventArgs.Channel.Guild.CreateVoiceChannelAsync($"╠🥳» {eventArgs.User.Username}'s Lounge",
+            eventArgs.Channel.Parent, 128000, position: 9999, user_limit: 4);
+            
+        if (newChannel == null)
+            return;
+            
+        eventArgs.Channel.Guild.Members.TryGetValue(eventArgs.User.Id, out var discordMember);
+        if (discordMember == null)
+            return;
+            
         await  Task.Run(async () =>
         {
-            var newChannel = await eventArgs.Channel.Guild.CreateVoiceChannelAsync($"╠🥳» {eventArgs.User.Username}'s Lounge",
-                eventArgs.Channel.Parent, 128000, position: 9999, user_limit: 4);
-            
-            if (newChannel == null)
-                return;
-            
-            eventArgs.Channel.Guild.Members.TryGetValue(eventArgs.User.Id, out var discordMember);
-            if (discordMember == null)
-                return;
-            
             await discordMember.PlaceInAsync(newChannel);
+        });
 
+        await  Task.Run(async () =>
+        {
             await newChannel.AddOverwriteAsync(discordMember, Permissions.ManageChannels);
+        });
+        
+        await  Task.Run(async () =>
+        {
             await newChannel.AddOverwriteAsync(discordMember, Permissions.AccessChannels);
+        });
+        
+        await  Task.Run(async () =>
+        {
             await newChannel.AddOverwriteAsync(discordMember, Permissions.UseVoice);
         });
+        
+                    
+        
+
     }
     
     public static async Task VoiceStateUpdated_ChanelLeave(DiscordClient client, VoiceStateUpdateEventArgs eventArgs)

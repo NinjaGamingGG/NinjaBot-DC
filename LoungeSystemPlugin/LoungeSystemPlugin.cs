1816472d634e6d0bee6f41c1a12a6191bd66705f
Taschner <imortaltrix@proton.me>
2023-06-14 13:01:17 +0200

    Lounge System Plugin Initial commit

---
+++ LoungeSystemPlugin/LoungeSystemPlugin.cs
@@ -0,0 +1,28 @@
+﻿using NinjaBot_DC;
+using PluginBase;
+using Serilog;
+
+namespace LoungeSystemPlugin;
+
+public class LoungeSystemPlugin : IPlugin
+{
+    public string Name => "LoungeSystem Plugin";
+    public string Description => "Simple Discord LoungeSystem Plugin.";
+    public string? PluginDirectory { get; set; }
+    public void OnLoad()
+    {
+        var client = Worker.GetServiceDiscordClient();
+        
+        if(ReferenceEquals(PluginDirectory, null))
+            OnUnload();
+
+        Directory.CreateDirectory(PluginDirectory!);
+        
+        Log.Information("[{Name}] Plugin Loaded", Name);
+    }
+
+    public void OnUnload()
+    {
+        Log.Information("[{Name}] Plugin Unloaded", Name);
+    }
+}
---
+++ LoungeSystemPlugin/PluginHelper/SqLiteHelper.cs
@@ -0,0 +1,44 @@
+﻿using System.Data.SQLite;
+using Microsoft.Extensions.Configuration;
+using Serilog;
+
+namespace LoungeSystemPlugin.PluginHelper;
+
+public static class SqLiteHelper
+{
+    private static SQLiteConnection? _sqLiteConnection;
+
+
+    public static void OpenSqLiteConnection(string pluginDirectory)
+    {
+        var configuration = ConfigHelper.Load();
+
+        var dataSource = configuration.GetValue<string>("lounge_system-plugin:sqlite-source") ?? "database.db";
+
+        var sqliteSource = Path.Combine(pluginDirectory, dataSource);
+
+        _sqLiteConnection = new SQLiteConnection($"Data Source={sqliteSource};Version=3;New=True;Compress=True;");
+
+        _sqLiteConnection.Open();
+    }
+    
+    public static SQLiteConnection? GetSqLiteConnection()
+    {
+        return _sqLiteConnection;
+    }
+    
+    public static void CloseSqLiteConnection()
+    {
+        _sqLiteConnection?.Close();
+    }
+
+    public static void InitializeSqliteTables()
+    {
+        if (_sqLiteConnection == null)
+        {
+            Log.Error("[LoungeSystem Plugin] SQLite connection is null, cannot initialize tables");
+            return;
+        }
+
+    }
+}

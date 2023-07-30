a5d040700fd05cbf2ad14ded99a373e25877cf9d
Taschner <Luca@Taschner.me>
2023-07-30 14:05:59 +0200

    Minimum Level for logger is now only set do debug if the application is run in debug configuration

--- NinjaBot-DC/LoggingBuilderExtensions.cs
+++ NinjaBot-DC/LoggingBuilderExtensions.cs
@@ -6,6 +6,8 @@ public static class LoggingBuilderExtensions
 {
     public static ILoggingBuilder ConfigureSerilog(this ILoggingBuilder loggingBuilder, IConfiguration configuration)
     {
+        
+#if DEBUG
         Log.Logger = new LoggerConfiguration()
             .ReadFrom.Configuration(configuration)
             .MinimumLevel.Debug()
@@ -14,5 +16,17 @@ public static class LoggingBuilderExtensions
             .CreateLogger();
 
         return loggingBuilder;
+#else
+        Log.Logger = new LoggerConfiguration()
+            .ReadFrom.Configuration(configuration)
+            .MinimumLevel.Information()
+            .WriteTo.Console()
+            .WriteTo.File(Path.Combine(Directory.GetCurrentDirectory(), "service.log"))
+            .CreateLogger();
+        
+                return loggingBuilder;
+#endif
+
+
     }
 }

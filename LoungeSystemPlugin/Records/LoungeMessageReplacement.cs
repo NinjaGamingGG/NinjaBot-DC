f671bdd49fb14846be2d15ac55de4ff1d1f45b7e
Taschner <Luca@Taschner.me>
2023-08-02 18:21:06 +0200

    Made strings nullable and changed type from class to record

--- LoungeSystemPlugin/Records/LoungeMessageReplacement.cs
+++ LoungeSystemPlugin/Records/LoungeMessageReplacement.cs
@@ -3,13 +3,13 @@ using Dapper.Contrib.Extensions;
 namespace LoungeSystemPlugin.Records;
 
 [Table("LoungeMessageReplacementIndex")]
-public class LoungeMessageReplacement
+public record LoungeMessageReplacement
 {
     [ExplicitKey]
     public int Id { get; set; }
     public ulong GuildId { get; set; }
     public ulong TargetChannelId { get; set; }
-    public string ReplacementHandle { get; set; }
-    public string ReplacementValue { get; set; } 
+    public string? ReplacementHandle { get; set; }
+    public string? ReplacementValue { get; set; } 
     
 }

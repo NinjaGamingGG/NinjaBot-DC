b441e626a2fa7b492e6fa9c1529aa6ade1fb5e8e
Luca <Luca@Taschner.me>
2023-10-15 17:06:20 +0200

    Fixed implementation in event

--- GreeterPlugin/Events/GuildMemberAdded.cs
+++ GreeterPlugin/Events/GuildMemberAdded.cs
@@ -11,67 +11,68 @@ namespace GreeterPlugin.Events;
 public static class GuildMemberAdded
 {
     
-    public static async Task GuildMemberAddedEvent(DiscordClient sender, GuildMemberAddEventArgs args)
+    public static async Task GuildMemberAddedEvent(DiscordClient client, GuildMemberAddEventArgs args)
     {
         var connection = MySqlConnectionHelper.GetMySqlConnection();
-        
-        var guildSettingsRecord = await connection.QueryFirstOrDefaultAsync<GuildSettingsRecord>("SELECT * FROM GuildSettingsIndex WHERE GuildId = @GuildId", new {GuildId = args.Guild.Id});
-        
-        if (guildSettingsRecord == null)
-        {
-            return;
-        }
-        
-        var userJoinedDataRecord = await connection.QueryFirstOrDefaultAsync<UserJoinedDataRecord>("SELECT * FROM UserJoinedDataIndex WHERE GuildId = @GuildId AND UserId = @UserId", new {GuildId = args.Guild.Id, UserId = args.Member.Id});
-        
-        if (userJoinedDataRecord == null)
-        {
-            var rowsInTable = await connection.ExecuteScalarAsync<int>("SELECT COUNT(*) FROM UserJoinedDataIndex WHERE GuildId = @GuildId", new {GuildId = args.Guild.Id});
             
-            userJoinedDataRecord = new UserJoinedDataRecord()
+            var guildSettingsRecord = await connection.QueryFirstOrDefaultAsync<GuildSettingsRecord>("SELECT * FROM GuildSettingsIndex WHERE GuildId = @GuildId", new {GuildId = args.Guild.Id});
+            
+            if (guildSettingsRecord == null)
             {
-                UserId = args.Member.Id,
-                GuildId = args.Guild.Id,
-                UserIndex = rowsInTable + 1,
-                WasGreeted = false
-            };
-        }
-        
-        var welcomeChannel = args.Guild.GetChannel(guildSettingsRecord.WelcomeChannelId);
-
-        var welcomeCard = GreeterPlugin.StaticPluginDirectory + $@"\welcomeCard{args.Member.Id}.png";
-        
-        GenerateWelcomeImage.Generator(args.Member.Username,
-            args.Member.AvatarUrl, 
-            guildSettingsRecord.WelcomeImageText,
-            userJoinedDataRecord.UserIndex,
-            guildSettingsRecord.WelcomeImageUrl, 
-            true, 
-            guildSettingsRecord.ProfilePictureOffsetX, 
-            guildSettingsRecord.ProfilePictureOffsetY, 
-            welcomeCard);
-        
-        var messageBuilder = new DiscordMessageBuilder();
+                return;
+            }
             
-        var filestream = File.Open(welcomeCard, FileMode.Open);
-            
-        messageBuilder.AddFile(filestream);
-                
-        await sender.SendMessageAsync(welcomeChannel, messageBuilder);
-
-        if (!IsFileLocked.Check(welcomeCard, 10))
-        {
-            File.Delete(welcomeCard);
-
-        }
-        else
-        {
-            Log.Error("[Greeter Plugin] Failed to delete welcome card, file appears to be locked! Filepath: {FilePath}", welcomeCard);
-                
-        } 
+            var userJoinedDataRecord = await connection.QueryFirstOrDefaultAsync<UserJoinedDataRecord>("SELECT * FROM UserJoinedDataIndex WHERE GuildId = @GuildId AND UserId = @UserId", new {GuildId = args.Guild.Id, UserId = args.Member.Id});
         
-        await connection.ExecuteAsync("UPDATE UserJoinedDataIndex SET WasGreeted = @WasGreeted WHERE GuildId = @GuildId AND UserId = @UserId", new {WasGreeted = true, GuildId = args.Guild.Id, UserId = args.Member.Id});
+            if (userJoinedDataRecord == null)
+            {
+                return;
+            }
+            
+            var welcomeChannel = args.Guild.GetChannel(guildSettingsRecord.WelcomeChannelId);
 
+            var welcomeCard = Path.Combine(GreeterPlugin.StaticPluginDirectory,"temp", $"welcomeCard{args.Member.Id}.png");
 
+            await GenerateWelcomeImage.Generator(args.Member.Username,
+                args.Member.AvatarUrl, 
+                guildSettingsRecord.WelcomeImageText,
+                userJoinedDataRecord.UserIndex,
+                guildSettingsRecord.WelcomeImageUrl, 
+                true, 
+                guildSettingsRecord.ProfilePictureOffsetX, 
+                guildSettingsRecord.ProfilePictureOffsetY, 
+                welcomeCard);
+            
+            var messageBuilder = new DiscordMessageBuilder();
+
+            var filestream = File.Open(welcomeCard,FileMode.Open);
+            
+            messageBuilder.AddFile(filestream);
+            
+            if (guildSettingsRecord.WelcomeMessage.Contains("{usermention}"))
+            {
+                guildSettingsRecord.WelcomeMessage = guildSettingsRecord.WelcomeMessage.Replace("{usermention}", args.Member.Mention);
+            }
+            
+            messageBuilder.WithContent(guildSettingsRecord.WelcomeMessage);
+                
+            await client.SendMessageAsync(welcomeChannel, messageBuilder);
+            
+            filestream.Close();
+            await filestream.DisposeAsync();
+
+            if (!IsFileLocked.Check(welcomeCard, 10))
+            {
+                File.Delete(welcomeCard);
+
+            }
+            else
+            {
+                Log.Error("[Greeter Plugin] Failed to delete welcome card, file appears to be locked! Filepath: {FilePath}", welcomeCard);
+                
+            } 
+            
+            await connection.ExecuteAsync("UPDATE UserJoinedDataIndex SET WasGreeted = @WasGreeted WHERE GuildId = @GuildId AND UserId = @UserId", new {WasGreeted = true, GuildId = args.Guild.Id, UserId = args.Member.Id});
+            
     }
 }

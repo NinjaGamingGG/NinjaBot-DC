6e16ecf57d6c1f82523596aebed924328b07f613
Luca Taschner <iMortalTrix@Proton.me>
2024-02-18 14:43:33 +0100

    Optimize ConfigurationBuilder initialization process
    Reorganized the code to initialize the ConfigurationBuilder outside of the condition block. This change ensures that the ConfigurationBuilder is readily available and reduces redundancy. It also enhances the code's readability and maintainability.

--- NinjaBot-DC/CommonPluginHelpers/ConfigHelper.cs
+++ NinjaBot-DC/CommonPluginHelpers/ConfigHelper.cs
@@ -8,18 +8,19 @@ public static class ConfigHelper
         //Check if there are any env variables set by loading the most mandatory variable
         var testLoad = Environment.GetEnvironmentVariables();
         
+        var builder = new ConfigurationBuilder();
+        
         //If none are found try to read from config files
         if (!testLoad.Contains(pluginEnvironmentVariablePrefix))
         {
 
             
-            return new ConfigurationBuilder()
+            return builder
                 .SetBasePath(pluginBasePath)
                 .AddJsonFile(Path.Combine(pluginBasePath,"config.json"), optional: false, reloadOnChange: true)
                 .Build();
         }
-
-        var builder = new ConfigurationBuilder();
+        
         builder.AddEnvironmentVariables();
         
         //If env vars are set load them

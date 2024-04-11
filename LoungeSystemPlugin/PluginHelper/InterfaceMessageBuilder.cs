2c8328f6b5a0350f7d2ed144cc7aa213e0c4b578
Luca Taschner <iMortalTrix@Proton.me>
2024-04-11 19:43:24 +0200

    Add InterfaceMessageBuilder to create interface messages
    The implementation of InterfaceMessageBuilder class externalizes the creation of interface messages from the 'VoiceStateUpdated' class. It provides a reusable method to pre-configure a DiscordMessageBuilder instance with message content and button components. This change streamlines the logic in the 'VoiceStateUpdated' event handling method by replacing the direct message building process with a method call.

---
+++ LoungeSystemPlugin/PluginHelper/InterfaceMessageBuilder.cs
@@ -0,0 +1,46 @@
+﻿using DSharpPlus;
+using DSharpPlus.Entities;
+
+namespace LoungeSystemPlugin.PluginHelper;
+
+/// <summary>
+/// Provides methods to build interface messages for the lounge system plugin.
+/// </summary>
+public static class InterfaceMessageBuilder
+{
+    /// <summary>
+    /// Returns a DiscordMessageBuilder instance with pre-configured message content and button components.
+    /// </summary>
+    /// <param name="client">The DiscordClient instance used to construct button components.</param>
+    /// <param name="messageContent">The message content to be set for the DiscordMessageBuilder instance.</param>
+    /// <returns>A DiscordMessageBuilder instance with pre-configured message content and button components.</returns>
+    public static DiscordMessageBuilder GetBuilder(DiscordClient client,string messageContent)
+    {
+       return new DiscordMessageBuilder()
+                .WithContent(messageContent)
+                .AddComponents([
+                    new DiscordButtonComponent(ButtonStyle.Secondary, "lounge_rename_button",
+                          "Rename",false, new DiscordComponentEmoji( DiscordEmoji.FromName(client, ":black_nib:"))),
+                    new DiscordButtonComponent(ButtonStyle.Secondary, "lounge_resize_button",
+                        "Resize", false, new DiscordComponentEmoji(DiscordEmoji.FromName(client, ":busts_in_silhouette:"))),
+                    new DiscordButtonComponent(ButtonStyle.Secondary, "lounge_trust_button",
+                        "Trust", false, new DiscordComponentEmoji(DiscordEmoji.FromName(client, ":people_hugging:")))
+                ])
+                .AddComponents([
+                    new DiscordButtonComponent(ButtonStyle.Secondary, "lounge_claim_button",
+                        "Claim", false, new DiscordComponentEmoji(DiscordEmoji.FromName(client, ":triangular_flag_on_post:"))),
+                    new DiscordButtonComponent(ButtonStyle.Secondary, "lounge_kick_button",
+                        "Kick", false, new DiscordComponentEmoji(DiscordEmoji.FromName(client, ":athletic_shoe:"))),
+                    new DiscordButtonComponent(ButtonStyle.Secondary, "lounge_un-trust_button",
+                        "Un-Trust", false, new DiscordComponentEmoji(DiscordEmoji.FromName(client, ":bust_in_silhouette:")))
+                ])
+                .AddComponents([
+                    new DiscordButtonComponent(ButtonStyle.Danger, "lounge_lock_button",
+                        "Un/Lock", false, new DiscordComponentEmoji(DiscordEmoji.FromName(client, ":lock:"))),
+                    new DiscordButtonComponent(ButtonStyle.Danger, "lounge_ban_button",
+                        "Ban", false, new DiscordComponentEmoji(DiscordEmoji.FromName(client, ":judge:"))),
+                    new DiscordButtonComponent(ButtonStyle.Danger, "lounge_delete_button",
+                        "Delete", false, new DiscordComponentEmoji(DiscordEmoji.FromName(client, ":put_litter_in_its_place:")))
+                ]);
+    }
+}

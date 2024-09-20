6f6acb1f9b73e217e02835e5d1b916aad0c0254d
Taschner <admin@NinjaGaming.gg>
2024-09-20 14:55:42 +0200

    Add Redis caching for lounge setup responses
    Implemented a Redis-based caching mechanism to store lounge setup responses in the AdminCommandsGroup class. This addition aids in retrieving and expiring new lounge configuration records efficiently. Also included error handling and logging to manage potential issues during the caching process.

--- LoungeSystemPlugin/CommandModules/AdminCommandsGroup.cs
+++ LoungeSystemPlugin/CommandModules/AdminCommandsGroup.cs
@@ -2,9 +2,12 @@
 using DSharpPlus.Commands;
 using DSharpPlus.Entities;
 using LoungeSystemPlugin.PluginHelper;
+using LoungeSystemPlugin.Records.Cache;
 using LoungeSystemPlugin.Records.MySQL;
 using MySqlConnector;
+using NRedisStack.RedisStackCommands;
 using Serilog;
+using StackExchange.Redis;
 
 namespace LoungeSystemPlugin.CommandModules;
 
@@ -101,6 +104,31 @@ public static class AdminCommandsGroup
         }
         
         await context.RespondAsync(LoungeSetupUiHelper.InitialMessageBuilder);
+        
+        var responseMessage = await context.GetResponseAsync();
+
+        if (ReferenceEquals(responseMessage, null))
+        {
+            Log.Error("[LoungeSystem Plugin] Unable to get response from Discord!");
+        }
+
+        try
+        {
+            var redisConnection = await ConnectionMultiplexer.ConnectAsync(LoungeSystemPlugin.RedisConnectionString);
+            var redisDatabase = redisConnection.GetDatabase(LoungeSystemPlugin.RedisDatabase);
+            var json = redisDatabase.JSON();
+            
+            var newLoungeSetupRecord = new LoungeSetupRecord(0, context.User.Id, "", "", true);
+            json.Set(responseMessage!.Id.ToString(), "$", newLoungeSetupRecord);
+            redisDatabase.KeyExpire(responseMessage.Id.ToString(), TimeSpan.FromMinutes(10));
+        }
+        catch (Exception ex)
+        {
+            Log.Error(ex, "[LoungeSystem Plugin]Unable to insert new configuration record!");
+        }
+
+
+        
     }
     
     

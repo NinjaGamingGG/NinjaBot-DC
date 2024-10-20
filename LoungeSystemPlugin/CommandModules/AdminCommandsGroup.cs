fbc528fd06d1c0f598b2dfb65783a1c677d58524
Taschner <admin@NinjaGaming.gg>
2024-10-20 19:03:51 +0200

    Remove unused imports and obsolete setup-noui command
    Removed unneeded using directives related to MySQL and Dapper. Deleted the obsolete LoungeSetup_NoUICommand to clean up the code and reduce complexity.

--- LoungeSystemPlugin/CommandModules/AdminCommandsGroup.cs
+++ LoungeSystemPlugin/CommandModules/AdminCommandsGroup.cs
@@ -1,10 +1,7 @@
-﻿using Dapper;
-using DSharpPlus.Commands;
+﻿using DSharpPlus.Commands;
 using DSharpPlus.Entities;
 using LoungeSystemPlugin.PluginHelper;
 using LoungeSystemPlugin.Records.Cache;
-using LoungeSystemPlugin.Records.MySQL;
-using MySqlConnector;
 using NRedisStack.RedisStackCommands;
 using Serilog;
 using StackExchange.Redis;
@@ -14,79 +11,6 @@ namespace LoungeSystemPlugin.CommandModules;
 [Command("lounge")]
 public static class AdminCommandsGroup
 {
-    
-    [Command("setup-noui")]
-    public static async Task LoungeSetup_NoUICommand(CommandContext context, DiscordChannel targetChannel, string namePattern, bool createInterfaceChannel = false, DiscordChannel? interfaceChannel = null)
-    {
-        await context.DeferResponseAsync();
-        
-        
-        if (createInterfaceChannel == false && ReferenceEquals(interfaceChannel, null))
-        {
-            await context.EditResponseAsync(new DiscordWebhookBuilder().WithContent(
-                "You need to specify an interface channel if you selected *false* for the interface option!"));
-            return;
-        }
-        
-        var connectionString = LoungeSystemPlugin.MySqlConnectionHelper.GetMySqlConnectionString();
-        int insertSuccess;
-        try
-        {
-            var mySqlConnection = new MySqlConnection(connectionString);
-            await mySqlConnection.OpenAsync();
-        
-            if (ReferenceEquals(mySqlConnection, null))
-            {
-                Log.Error("[{PluginName}] Unable to connect to database!",LoungeSystemPlugin.GetStaticPluginName());
-                return;
-            }
-
-            var guildId = context.Channel.Guild.Id;
-
-            var newConfigRecord = new LoungeSystemConfigurationRecord()
-            {
-                GuildId = guildId,
-                TargetChannelId = targetChannel.Id,
-                InterfaceChannelId = interfaceChannel?.Id ?? 0,
-                LoungeNamePattern = namePattern
-            };
-        
-            var alreadyExists = await mySqlConnection.ExecuteScalarAsync<int>(
-                "SELECT COUNT(*) FROM LoungeSystemConfigurationIndex WHERE GuildId = @GuildId AND TargetChannelId = @TargetChannelId",
-                new { GuildId = guildId, TargetChannelId = targetChannel.Id });
-        
-            if (alreadyExists != 0)
-            {
-                await context.EditResponseAsync(
-                    new DiscordWebhookBuilder().WithContent("Error. Configuration already exists!"));
-                return;
-            }
-        
-            insertSuccess = await mySqlConnection.ExecuteAsync(
-                "INSERT INTO LoungeSystemConfigurationIndex (GuildId, TargetChannelId, InterfaceChannelId, LoungeNamePattern) VALUES (@GuildId, @TargetChannelId, @InterfaceChannelId, @LoungeNamePattern)",
-                newConfigRecord);
-        
-            await mySqlConnection.CloseAsync();
-        }
-        catch (Exception e)
-        {
-            Log.Error(e, "[{PluginName}] Error while Executing Mysql Operations on Lounge System Admin Command Module Setup Command", LoungeSystemPlugin.GetStaticPluginName());
-            return;
-        }
-        
-
-        if (insertSuccess == 0)
-        {
-            await context.EditResponseAsync(
-                new DiscordWebhookBuilder().WithContent("Error. Unable to insert new configuration record!"));
-            return;
-        }
-        
-        await context.EditResponseAsync(
-            new DiscordWebhookBuilder().WithContent("Successfully created new configuration record!"));
-        
-    }
-
     [Command("setup")]
     public static async Task LoungeSetupCommand(CommandContext context)
     {
@@ -127,10 +51,5 @@ public static class AdminCommandsGroup
         {
             Log.Error(ex, "[{PluginName}] Unable to insert new configuration record!",LoungeSystemPlugin.GetStaticPluginName());
         }
-
-
-        
     }
-    
-    
 }

46f5d3bbe3df6b02c398da242c7c02e77d463994
Taschner <admin@NinjaGaming.gg>
2024-10-24 17:10:30 +0200

    Add profanity check in lounge rename modal
    Implemented a check to detect profanity in the new lounge name before making any changes. This ensures that inappropriate names are not set, providing a better user experience. Logging and error handling are added to manage exceptions during the check.

--- LoungeSystemPlugin/Events/ModalsSubmitted/LoungeRenameModal.cs
+++ LoungeSystemPlugin/Events/ModalsSubmitted/LoungeRenameModal.cs
@@ -1,7 +1,9 @@
 ﻿using DSharpPlus;
+using DSharpPlus.Entities;
 using DSharpPlus.EventArgs;
 using DSharpPlus.Net.Models;
 using LoungeSystemPlugin.PluginHelper;
+using Serilog;
 
 namespace LoungeSystemPlugin.Events.ModalsSubmitted;
 
@@ -12,9 +14,30 @@ public static class LoungeRenameModal
         await eventArgs.Interaction.DeferAsync();
         
         var isValuePresent = eventArgs.Values.TryGetValue("lounge_new_name", out var userValue);
-
+        
         if (isValuePresent == false || string.IsNullOrEmpty(userValue) )
             return;
+        
+        bool containsProfanity;
+        
+        try
+        {
+            containsProfanity  = await ProfanityCheck.CheckString(userValue);
+        }
+        catch (Exception ex)
+        {
+            Log.Error(ex,"[{PluginName}] Error while checking for Profanity", LoungeSystemPlugin.GetStaticPluginName());
+            await eventArgs.Interaction.EditOriginalResponseAsync(
+                new DiscordWebhookBuilder()
+                    .WithContent("Interaction Failed, please try again later."));
+            return;
+        }
+
+        if (containsProfanity)
+        {
+            await eventArgs.Interaction.EditOriginalResponseAsync(new DiscordWebhookBuilder().WithContent("Unable to change the name to the Provided string due to Profanity."));
+            return;
+        }
         
         if (ReferenceEquals(eventArgs.Interaction.Guild, null))
             return;
@@ -24,7 +47,6 @@ public static class LoungeRenameModal
         
         var channel = await sender.GetChannelAsync(eventArgs.Interaction.Channel.Id);
         await channel.ModifyAsync(NewEditModel);
-        
         
         await eventArgs.Interaction.DeleteOriginalResponseAsync();
         return;
